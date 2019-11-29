70b275e4fb0495782bc2331327a94c61a9bb718a
tenasatupitsyn <214782@bk.ru>
2019-11-29 01:50:06 -0300

    Verify that the server is already part of the database

--- core/events/guildCreate.js
+++ core/events/guildCreate.js
@@ -1,6 +1,9 @@
 const { GuildController } = require('../database/controllers');
 
-module.exports = (Juge, guild) => {
+module.exports = async (Juge, guild) => {
+	const guildExists = await GuildController.findOne(guild);
+	if (guildExists) return;
+
 	GuildController.add(guild)
 		.then(() => Juge.log(`${guild.name} (${guild.id})`, 'guildCreate'))
 		.catch((error) => Juge.log(error.message, 'guildCreate'));
