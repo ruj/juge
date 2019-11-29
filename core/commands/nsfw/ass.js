19e77bfba0b2afd7fdb974a3f02d0827e1d7c9eb
tenasatupitsyn <214782@bk.ru>
2019-11-29 01:44:39 -0300

    noise

--- core/commands/nsfw/ass.js
+++ core/commands/nsfw/ass.js
@@ -1,24 +1,32 @@
+const Butts = require('../../apis/OpenButts.js');
+
 module.exports = {
 	name: 'ass',
 	aliases: ['butt', 'booty', 'butts'],
 	permissions: ['EMBED_LINKS'],
 	permissionLevel: 0,
 	description: 'Butt images',
-	usage: '',
+	usage: '<noise>',
 	category: 'nsfw',
 	guildOnly: false,
 	params: false,
 	cooldown: 5,
 	enabled: true,
 	async execute(Juge, message, params) {
-		const Butts = require('../../apis/OpenButts.js');
-
 		try {
-			const butts = await Butts.getButts();
-			const embed = new Juge.RichEmbed()
-				.setColor(Juge.util.hexColor.embed(message))
-				.setImage(`http://media.obutts.ru/${butts[0].preview}`)
-			message.channel.send(embed);
+			if (params.length < 1) {
+				const butts = await Butts.getButts();
+				const embed = new Juge.RichEmbed()
+					.setColor(Juge.util.hexColor.embed(message))
+					.setImage(`http://media.obutts.ru/${butts[0].preview}`)
+				message.channel.send(embed);
+			} else if (params[0].toUpperCase() === 'NOISE') {
+				const noise = await Butts.getNoise();
+				const embed = new Juge.RichEmbed()
+					.setColor(Juge.util.hexColor.embed(message))
+					.setImage(`http://media.obutts.ru/${noise[0].preview}`)
+				message.channel.send(embed);
+			}
 		} catch (error) {
 			const embed = new Juge.RichEmbed()
 				.setColor(Juge.util.hexColor.error)
