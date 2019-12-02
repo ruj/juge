28df2ac0fe9e74163d58424c0817ba37867c7d9e
tenasatupitsyn <214782@bk.ru>
2019-12-02 10:14:27 -0300

    Booru as a dependency

---
+++ core/commands/nsfw/rule34.js
@@ -0,0 +1,41 @@
+const Booru = require('booru');
+
+module.exports = {
+	name: 'rule34',
+	aliases: ['r34'],
+	permissions: ['EMBED_LINKS'],
+	permissionLevel: 0,
+	description: 'Searches for images on rule34.xxx',
+	usage: '[search query]',
+	category: 'nsfw',
+	guildOnly: false,
+	params: true,
+	cooldown: 10,
+	enabled: true,
+	async execute(Juge, message, params) {
+		if (!message.content.toUpperCase().includes('LOLI', 'GORE')) {
+			Booru.search('rule34', [ params.join('_') ], { limit: 1, random: true })
+				.then((posts) => {
+					if (posts.length) {
+						const embed = new Juge.RichEmbed()
+							.setColor(Juge.util.hexColor.embed(message))
+							.setImage(posts[0].fileUrl)
+						message.channel.send(embed);
+					} else {
+						throw new Error('No results found');
+					}
+				})
+				.catch((error) => {
+					const embed = new Juge.RichEmbed()
+						.setColor(Juge.util.hexColor.error)
+						.setDescription(`:x: : Oops, **${error.message}**.`)
+					message.channel.send(embed);
+				});
+		} else {
+			const embed = new Juge.RichEmbed()
+				.setColor(Juge.util.hexColor.warning)
+				.setDescription(':warning: : I can not send it here, not even on NSFW channels.')
+			message.channel.send(embed);
+		}
+	}
+};
