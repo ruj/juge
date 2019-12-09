6675084ca2c906d14a14abb623202e12ad1037d5
tenasatupitsyn <214782@bk.ru>
2019-12-09 18:56:37 -0300

    High definition content searcher

---
+++ core/commands/nsfw/4k.js
@@ -0,0 +1,29 @@
+const Neko = require('../../apis/NekoBot.js');
+
+module.exports = {
+	name: '4k',
+	aliases: ['hq'],
+	permissions: ['EMBED_LINKS'],
+	permissionLevel: 0,
+	description: 'High quality NSFW content',
+	usage: '',
+	category: 'nsfw',
+	guildOnly: false,
+	params: false,
+	cooldown: 5,
+	enabled: true,
+	async execute(Juge, message, params) {
+		try {
+			const neko = await Neko.image(this.name);
+			const embed = new Juge.RichEmbed()
+				.setColor(Juge.util.hexColor.embed(message))
+				.setImage(neko.message)
+			message.channel.send(embed);
+		} catch (error) {
+			const embed = new Juge.RichEmbed()
+				.setColor(Juge.util.hexColor.error)
+				.setDescription(`:x: : Oops, **${error.message}**`)
+			message.channel.send(embed);
+		}
+	}
+};
