c060a938ff17bca5099846aa2bf19ebcc4a5c7c2
tenasatupitsyn <214782@bk.ru>
2019-12-09 19:00:29 -0300

    Nekos!

---
+++ core/commands/nsfw/neko.js
@@ -0,0 +1,29 @@
+const Neko = require('../../apis/NekosDotLife.js');
+
+module.exports = {
+	name: 'neko',
+	aliases: ['nekos', 'catgirl'],
+	permissions: ['EMBED_LINKS'],
+	permissionLevel: 0,
+	description: 'Nekos!',
+	usage: '',
+	category: 'nsfw',
+	guildOnly: false,
+	params: false,
+	cooldown: 5,
+	enabled: true,
+	async execute(Juge, message, params) {
+		try {
+			const neko = await Neko.image(this.name);
+			const embed = new Juge.RichEmbed()
+				.setColor(Juge.util.hexColor.embed(message))
+				.setImage(neko.url)
+			message.channel.send(embed);
+		} catch (error) {
+			const embed = new Juge.RichEmbed()
+				.setColor(Juge.util.hexColor.error)
+				.setDescription(`:x: : Oops, **${error.message}**`)
+			message.channel.send(embed);
+		}
+	}
+};
