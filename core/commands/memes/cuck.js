f1cd6b67e4d3a3626b0bee4e2388d8d6fff49870
tenasatupitsyn <214782@bk.ru>
2019-12-15 22:30:09 -0300

    memes

---
+++ core/commands/memes/cuck.js
@@ -0,0 +1,32 @@
+const { resolve } = require('path');
+const Jimp = require('jimp');
+
+module.exports = {
+  name: 'cuck',
+  aliases: [],
+  permissions: ['EMBED_LINKS', 'ATTACH_FILES'],
+  permissionLevel: 0,
+  description: '',
+  usage: '<user>',
+  category: 'memes',
+  guildOnly: false,
+  params: false,
+  cooldown: 10,
+  enabled: true,
+  async execute(Juge, message, params) {
+    try {
+      message.channel.startTyping();
+      const avatar = message.mentions.users.size ? message.mentions.users.first().displayAvatarURL : message.author.displayAvatarURL;
+      const filter = await Jimp.read(resolve(__dirname, '..', '..', 'assets', 'png', 'cuck.png'));
+      const image = await Jimp.read(avatar);
+      image.resize(512, 512);
+      image.composite(filter, 0, 0);
+      await message.channel.send({ files: [{ attachment: await image.getBufferAsync(Jimp.MIME_PNG), name: 'cuck.png' }] }).then(message.channel.stopTyping());
+    } catch (error) {
+      const embed = new Juge.RichEmbed()
+        .setColor(Juge.util.hexColor.error)
+        .setDescription(`:x: : Oops, **${error.message}**`)
+      message.channel.send(embed).then(message.channel.stopTyping());
+    }
+  }
+};
