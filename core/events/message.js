f8602536c40faf74d528b2fe5d8cb98025931edc
tenasatupitsyn <214782@bk.ru>
2019-12-17 19:42:44 -0300

    Removed prefix search and now filters

--- core/events/message.js
+++ core/events/message.js
@@ -31,76 +31,78 @@ module.exports = async (Juge, message) => {
 					}
 				});
 
-				prefixes.find(async (prefix) => {
-					if (message.author.bot || !message.content.startsWith(prefix)) return;
+				prefixes.filter(async (prefix) => {
+          if (message.content.startsWith(prefix)) {
+            if (message.author.bot) return;
 
-					const params = message.content.slice(prefix.length).split(/ +/);
-					const commandName = params.shift().toLowerCase();
-					const command = Juge.commands.get(commandName) || Juge.commands.find((command) => command.aliases && command.aliases.includes(commandName));
-					const permission = Juge.elevation(message);
+            const params = message.content.slice(prefix.length).split(/ +/);
+            const commandName = params.shift().toLowerCase();
+            const command = Juge.commands.get(commandName) || Juge.commands.find((command) => command.aliases && command.aliases.includes(commandName));
+            const permission = Juge.elevation(message);
 
-					if (!command) return;
-					if (!message.guild.me.permissions.toArray().includes(command.permissions)) {
-						const reqPermissions = Juge.util.difference(command.permissions, message.guild.me.permissions.toArray());
-						if (reqPermissions.length > 0) return message.reply(`for this command to work I need the following permissions: \`${reqPermissions.join('\`, \`')}\`.`);
-					}
+            if (!command) return;
+            if (!message.guild.me.permissions.toArray().includes(command.permissions)) {
+              const reqPermissions = Juge.util.difference(command.permissions, message.guild.me.permissions.toArray());
+              if (reqPermissions.length > 0) return message.reply(`for this command to work I need the following permissions: \`${reqPermissions.join('\`, \`')}\`.`);
+            }
 
-					if (command.category === 'nsfw' && !message.channel.nsfw) {
-						const embed = new Juge.RichEmbed()
-							.setColor(Juge.util.hexColor.error)
-							.setTitle('NSFW Command')
-							.setDescription('Please switch to NSFW channel in order to use this command.')
-							.setImage('https://a.kyouko.se/m3cN.jpg')
-						return message.channel.send(embed);
-					}
+            if (command.category === 'nsfw' && !message.channel.nsfw) {
+              const embed = new Juge.RichEmbed()
+                .setColor(Juge.util.hexColor.error)
+                .setTitle('NSFW Command')
+                .setDescription('Please switch to NSFW channel in order to use this command.')
+                .setImage('https://a.kyouko.se/m3cN.jpg')
+              return message.channel.send(embed);
+            }
 
-					if (command.params && !params.length) {
-						return message.reply('you did not provide any parameters.')
-							.then(() => {
-								if (command.usage) {
-									const embed = new Juge.RichEmbed()
-										.setColor(Juge.util.hexColor.embed(message))
-										.setTitle('Usage')
-										.setDescription(`${prefix}${command.name} ${command.usage}`);
-									message.channel.send(embed);
-								}
-							});
-					}
+            if (command.params && !params.length) {
+              return message.reply('you did not provide any parameters.')
+                .then(() => {
+                  if (command.usage) {
+                    const embed = new Juge.RichEmbed()
+                      .setColor(Juge.util.hexColor.embed(message))
+                      .setTitle('Usage')
+                      .setDescription(`${prefix}${command.name} ${command.usage}`);
+                    message.channel.send(embed);
+                  }
+                });
+            }
 
-					if (permission < command.permissionLevel) return;
-					if (message.author.id !== Juge.config.ownerID && !command.enabled) return message.reply('sorry the command has been \`Disabled\`.');
+            if (permission < command.permissionLevel) return;
+            if (message.author.id !== Juge.config.ownerID && !command.enabled) return message.reply('sorry the command has been \`Disabled\`.');
 
-					if (!Juge.cooldowns.has(command.name)) {
-						Juge.cooldowns.set(command.name, new (require('discord.js')).Collection());
-					}
+            if (!Juge.cooldowns.has(command.name)) {
+              Juge.cooldowns.set(command.name, new (require('discord.js')).Collection());
+            }
 
-					const now = Date.now();
-					const timestamps = Juge.cooldowns.get(command.name);
-					const cooldownAmount = (command.cooldown || 3) * 1E3;
+            const now = Date.now();
+            const timestamps = Juge.cooldowns.get(command.name);
+            const cooldownAmount = (command.cooldown || 3) * 1E3;
 
-					if (timestamps.has(message.author.id)) {
-						const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-						if (now < expirationTime) {
-							const timeLeft = ((expirationTime - now) / 1E3).toFixed(1);
-							return message.reply(`please wait \`${timeLeft}\` more second(s).`).then((_message) => _message.delete(timeLeft * 1E3));
-						}
-					}
+            if (timestamps.has(message.author.id)) {
+              const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+              if (now < expirationTime) {
+                const timeLeft = ((expirationTime - now) / 1E3).toFixed(1);
+                return message.reply(`please wait \`${timeLeft}\` more second(s).`).then((_message) => _message.delete(timeLeft * 1E3));
+              }
+            }
 
-					timestamps.set(message.author.id, now);
-					setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+            timestamps.set(message.author.id, now);
+            setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
-					try {
-						const commandExists = await CommandController.findOne(command.name);
-						if (commandExists) {
-							await CommandController.update(command.name, { $set: { count: commandExists.count + 1 } });
-						} else {
-							await CommandController.add(command);
-						}
+            try {
+              const commandExists = await CommandController.findOne(command.name);
+              if (commandExists) {
+                await CommandController.update(command.name, { $set: { count: commandExists.count + 1 } });
+              } else {
+                await CommandController.add(command);
+              }
 
-						command.execute(Juge, message, params);
-					} catch (error) {
-						Juge.log(error.message, { tags: ['execute'], color: 'red' });
-					}
+              command.execute(Juge, message, params);
+            } catch (error) {
+              Juge.log(error.message, { tags: ['execute'], color: 'red' });
+            }
+          }
 				});
 			} else if (guild === null) {
 				const addGuild = await GuildController.add(message.guild);
