705ea787318cf2a539459a5294122d181866637e
tenasatupitsyn <214782@bk.ru>
2019-12-26 05:22:39 -0300

    Invitation generator

--- core/commands/bot/invite.js
+++ core/commands/bot/invite.js
@@ -1,7 +1,5 @@
 const invitationPhrases = require('../../assets/json/invitation_phrases.json');
 
-const INVITE_LINK = `https://discordapp.com/oauth2/authorize?client_id={{JUGE_ID}}&permissions=${process.env.JUGE_PERMISSIONS}&scope=bot`;
-
 module.exports = {
 	name: 'invite',
 	aliases: ['botinvite'],
@@ -14,12 +12,11 @@ module.exports = {
 	params: false,
 	cooldown: 0,
 	enabled: true,
-	execute(client, message, params) {
+	async execute(client, message, params) {
 		message.channel.send(new client.RichEmbed()
 			.setColor(client.util.hexColor(message))
 			.setDescription(client.util.randomItem(invitationPhrases)
-				.replace('{{INVITE_LINK}}', INVITE_LINK)
-				.replace('{{JUGE_ID}}', client.user.id)
+				.replace('{{INVITE_LINK}}', await client.generateInvite(Number(process.env.JUGE_PERMISSIONS)))
 			)
 		);
 	}
