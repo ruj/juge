47fc3abc53c044c8e906e259f8ca098f5488eaaa
tenasatupitsyn <214782@bk.ru>
2019-12-26 03:18:52 -0300

    Server runtime

--- core/commands/bot/uptime.js
+++ core/commands/bot/uptime.js
@@ -1,9 +1,13 @@
+const { readFileSync } = require('fs');
+const moment = require('moment');
+require('moment-duration-format');
+
 module.exports = {
   name: 'uptime',
   aliases: ['up'],
   permissions: ['EMBED_LINKS'],
   permissionLevel: 0,
-  description: 'Time the client is awake',
+  description: 'Time the client and server is awake',
   usage: '',
   category: 'bot',
   guildOnly: false,
@@ -11,9 +15,19 @@ module.exports = {
   cooldown: 0,
   enabled: true,
   execute(client, message, params) {
+    const uptime = process.env.NODE_ENV ? parseFloat(readFileSync('/proc/uptime', { encoding: 'utf-8' }).split(' ')[0] * 1E3) : undefined;
+    const duration = moment.duration(uptime);
+
+    let serverUptime;
+    if (duration.days() < 1) serverUptime = client.util.uptime(uptime);
+    else if (duration.days() == 1) serverUptime = duration.format('D [day]');
+    else if (duration.days() < 10) serverUptime = duration.format('D [days]');
+    else if (duration.days() >= 10) serverUptime = duration.format('DD [days]');
+
     message.channel.send(new client.RichEmbed()
       .setColor(client.util.hexColor(message))
-      .setDescription(`:clock4: : ${client.util.uptime(client.uptime)}`)
+      .addField(':clock4: Process', client.util.sendCode(client.util.uptime(client.uptime), { code: 'xl' }))
+      .addField(':desktop: Server', client.util.sendCode(!serverUptime.startsWith('NaN') ? serverUptime : ' ', { code: 'xl' }))
     );
   }
 };
