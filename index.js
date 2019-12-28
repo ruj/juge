0ca6cfaecb97f95debda6fc2d5f8fbf3b5bcf86c
tenasatupitsyn <214782@bk.ru>
2019-12-28 20:46:07 -0300

    Shard options changed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const Manager = new (require('discord.js')).ShardingManager('./core/Juge.js', {
-	totalShards: 1,
-	respawn: true,
-	token: process.env.JUGE_TOKEN
-});
+// Initialize shards
+const SHARD_OPTIONS = {
+  token: process.env.JUGE_TOKEN
+};
+
+const { ShardingManager } = require('discord.js');
+const Manager = new ShardingManager('./core/Juge.js', SHARD_OPTIONS);
 
 Manager.on('launch', (shard) => console.log('shard', `Launching shard ${shard.id + 1}/${Manager.totalShards}`));
 Manager.spawn();
