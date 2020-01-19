2798f7995fd8349906ef685df09545178c0ab3bf
tenasatupitsyn <214782@bk.ru>
2020-01-18 22:39:52 -0300

    Find specific command by route

--- core/http/api/commands.js
+++ core/http/api/commands.js
@@ -8,6 +8,14 @@ module.exports = {
       response.status(200).json(client.commands.filter(({ category }) => category !== 'developer').map((command) => command));
     });
 
+    router.get('/:name', (request, response) => {
+      const command = client.commands.find((command) => command.name === request.params.name);
+
+      if (!command) return response.status(400).json({ message: 'Invalid command' });
+
+      return response.status(200).json(command);
+    });
+
     app.use(path, router);
   }
 };
