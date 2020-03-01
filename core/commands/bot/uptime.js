3738b85dfd3c8838b81f3efc55c81bc96a616c8f
tenasatupitsyn <214782@bk.ru>
2020-03-01 03:01:04 -0300

    Shortened assignments and a unique function for that

--- core/commands/bot/uptime.js
+++ core/commands/bot/uptime.js
@@ -1,7 +1,6 @@
 const { readFileSync } = require('fs');
 const moment = require('moment');
 require('moment-duration-format');
-const _ = require('lodash');
 
 module.exports = {
   name: 'uptime',
@@ -9,23 +8,24 @@ module.exports = {
   description: 'Time the client and server is awake',
   category: 'bot',
   requirements: { botPermissions: ['EMBED_LINKS'] },
-  cooldown: 5,
   execute(client, message, params) {
     let uptime = {
       process: client.utils.uptime(process.uptime() * 1E3),
       shard: client.utils.uptime(client.uptime)
     };
 
+    const writeServerUptime = (server) => Object.assign(uptime, { server });
+
     if (process.env.NODE_ENV) {
       const proc = parseFloat(readFileSync('/proc/uptime', { encoding: 'utf-8' }).split(' ')[0] * 1E3);
       const duration = moment.duration(proc);
 
-      if (duration.days() < 1) _.assign(uptime, { server: client.utils.uptime(proc) });
-      else if (duration.days() == 1) _.assign(uptime, { server: duration.format('D [day]') });
-      else if (duration.days() < 10) _.assign(uptime, { server: duration.format('D [days]') });
-      else if (duration.days() >= 10) _.assign(uptime, { server: duration.format('DD [days]') });
+      if (duration.days() < 1) writeServerUptime(client.utils.uptime(proc));
+      else if (duration.days() == 1) writeServerUptime(duration.format('D [day]'));
+      else if (duration.days() < 10) writeServerUptime(duration.format('D [days]'));
+      else if (duration.days() >= 10) writeServerUptime(duration.format('DD [days]'));
     } else {
-      _.assign(uptime, { server: 'Not in production' });
+      writeServerUptime('Not in production');
     }
 
     message.channel.send(new client.RichEmbed()
