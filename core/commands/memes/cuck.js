2c7cf8fc1c850970bb0afb1824c1605e068197a6
tenasatupitsyn <214782@bk.ru>
2020-03-02 03:16:16 -0300

    Using the jimp utility and typing

--- core/commands/memes/cuck.js
+++ core/commands/memes/cuck.js
@@ -1,5 +1,4 @@
-const { resolve } = require('path');
-const Jimp = require('jimp');
+const { JimpUtils } = require('../../');
 
 module.exports = {
   name: 'cuck',
@@ -7,22 +6,16 @@ module.exports = {
   description: '',
   usage: '<user>',
   category: 'memes',
-  requirements: { botPermissions: ['EMBED_LINKS', 'ATTACH_FILES'] },
+  requirements: { botPermissions: ['EMBED_LINKS', 'ATTACH_FILES'], typing: true },
   cooldown: 10,
   async execute(client, message, params) {
-    try {
-      message.channel.startTyping();
-      const avatar = message.mentions.users.size ? message.mentions.users.first().displayAvatarURL : message.author.displayAvatarURL;
-      const filter = await Jimp.read(resolve(__dirname, '..', '..', 'assets', 'png', 'cuck.png'));
-      const image = await Jimp.read(avatar);
-      image.resize(512, 512);
-      image.composite(filter, 0, 0);
-      await message.channel.send({ files: [{ attachment: await image.getBufferAsync(Jimp.MIME_PNG), name: 'cuck.png' }] }).then(message.channel.stopTyping());
-    } catch (error) {
-      message.channel.send(new client.RichEmbed()
-        .setColor(client.utils.hexColor('ERROR'))
-        .setDescription(`:x: : Oops, **${error.message}**`)
-      ).then(message.channel.stopTyping());
-    }
+    const avatar = message.mentions.users.size ? message.mentions.users.first().displayAvatarURL : message.author.displayAvatarURL;
+
+    message.channel.send({
+      files: [{
+        attachment: await JimpUtils.composite(avatar, 'png/cuck.png', { size: 512 }),
+        name: 'cuck.png'
+      }]
+    });
   }
 };
--- core/commands/memes/disabled.js
+++ core/commands/memes/disabled.js
@@ -1,5 +1,4 @@
-const { resolve } = require('path');
-const Jimp = require('jimp');
+const { JimpUtils } = require('../../');
 
 module.exports = {
   name: 'disabled',
@@ -7,22 +6,21 @@ module.exports = {
   description: '',
   usage: '<user>',
   category: 'memes',
-  requirements: { botPermissions: ['EMBED_LINKS', 'ATTACH_FILES'] },
+  requirements: { botPermissions: ['EMBED_LINKS', 'ATTACH_FILES'], typing: true },
   cooldown: 10,
   async execute(client, message, params) {
-    try {
-      message.channel.startTyping();
-      const avatarURL = message.mentions.users.size ? message.mentions.users.first().displayAvatarURL : message.author.displayAvatarURL;
-      const disabled = await Jimp.read(resolve(__dirname, '..', '..', 'assets', 'jpg', 'disabled.jpg'));
-      const avatar = await Jimp.read(avatarURL);
-      avatar.resize(157, 157);
-      disabled.composite(avatar, 390, 252);
-      await message.channel.send({ files: [{ attachment: await disabled.getBufferAsync(Jimp.MIME_JPEG), name: 'disabled.jpg' }] }).then(message.channel.stopTyping());
-    } catch (error) {
-      message.channel.send(new client.RichEmbed()
-        .setColor(client.utils.hexColor('ERROR'))
-        .setDescription(`:x: : Oops, **${error.message}**`)
-      ).then(message.channel.stopTyping());
-    }
+    const avatar = message.mentions.users.size ? message.mentions.users.first().displayAvatarURL : message.author.displayAvatarURL;
+
+    message.channel.send({
+      files: [{
+        attachment: await JimpUtils.composite(avatar, 'jpg/disabled.jpg', {
+          size: 157,
+          position: [390, 252],
+          invertBase: true,
+          mime: 'jpg'
+        }),
+        name: 'disabled.jpg'
+      }]
+    });
   }
 };
