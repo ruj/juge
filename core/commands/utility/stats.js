7c97c15714e0b9e7da8d9c4f1218ef64ca3456f8
tenasatupitsyn <214782@bk.ru>
2020-03-02 16:55:14 -0300

    Statistics of times the command was used

---
+++ core/commands/utility/stats.js
@@ -0,0 +1,23 @@
+const { CommandRepository } = require('../../database/repositories');
+
+module.exports = {
+  name: 'stats',
+  aliases: [],
+  description: '',
+  category: 'utility',
+  requirements: { botPermissions: ['EMBED_LINKS'] },
+  cooldown: 15,
+  async execute(client, message, params) {
+    const commands = await CommandRepository.findAll();
+
+    message.channel.send(new client.RichEmbed()
+      .setColor(client.utils.hexColor(message))
+      .setTitle(':notepad_spiral: Command usage statistics')
+      .setDescription(commands
+        .sort((a, b) => b.count - a.count)
+        .slice(0, 15)
+        .map((command, index) => `\`${client.utils.leading(String(index + 1), { width: String(commands.length).length })}\` : **${command.name}** -- ${command.count}`)
+        .join('\n'))
+    );
+  }
+};
