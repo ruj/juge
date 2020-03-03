dea063402297c0634e2a1d829d455c91ca3be914
tenasatupitsyn <214782@bk.ru>
2020-03-03 02:27:22 -0300

    Gets commits from all branches and stable is standard

--- core/commands/utility/changelog.js
+++ core/commands/utility/changelog.js
@@ -5,16 +5,24 @@ module.exports = {
   name: 'changelog',
   aliases: ['updates', 'commits'],
   description: 'Latest commits to bot repository',
+  usage: '<branch>',
   category: 'utility',
   requirements: { botPermissions: ['EMBED_LINKS'] },
   cooldown: 60,
   async execute(client, message, params) {
-    const commits = (await GitHub.getRepository(JUGE_REPO_USERNAME, JUGE_REPO_NAME, 'commits')).slice(0, 10);
+    const [ branchName ] = params;
+    const branches = await GitHub.getRepository(JUGE_REPO_USERNAME, JUGE_REPO_NAME, 'branches');
+    let branch = branches.filter((branch) => branch.name === branchName.toLowerCase());
 
+    branch = !branch.length
+    ? branches.filter((branch) => branch.name === 'stable')[0]
+    : branch[0]
+
+    const commits = (await GitHub.getRepository(JUGE_REPO_USERNAME, JUGE_REPO_NAME, `commits?sha=${branch.commit.sha}&per_page=NEED_THIS`)).slice(0, 10);
     const embed = new client.MessageEmbed()
       .setColor(client.utils.hexColor(message))
-      .setTitle(`${JUGE_REPO_NAME}:master`)
-      .setURL(`https://github.com/${JUGE_REPO_USERNAME}/${JUGE_REPO_NAME}/commits/master`)
+      .setTitle(`${JUGE_REPO_NAME}:${branch.name}`)
+      .setURL(`https://github.com/${JUGE_REPO_USERNAME}/${JUGE_REPO_NAME}/commits/${branch.name}`)
 
       commits.map(({ sha, commit, html_url, committer }) => embed.addField(sha.slice(0, 7), [
         `[\`${commit.message}\`](${html_url}) (${client.utils.days(commit.committer.date, { extended: false }) > 1 ? `${client.utils.days(commit.committer.date)} ago` : 'Today'})`,
