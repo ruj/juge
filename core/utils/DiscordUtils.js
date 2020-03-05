b91b7b7faaad3a9eeca21e59d539d7dcd83258e8
tenasatupitsyn <214782@bk.ru>
2020-03-05 18:22:04 -0300

    Correction of parameters property

--- core/utils/DiscordUtils.js
+++ core/utils/DiscordUtils.js
@@ -2,18 +2,18 @@ module.exports = {
   resolveUser(message, id) {
     if (message.mentions.members.size) {
       id = message.mentions.members.first().id;
-    } else if (!isNaN(message.params[0])) {
-      if (message.guild.members.cache.get(message.params[0])) {
-        id = message.guild.members.cache.get(message.params[0]).id;
+    } else if (!isNaN(message.parameters[0])) {
+      if (message.guild.members.cache.get(message.parameters[0])) {
+        id = message.guild.members.cache.get(message.parameters[0]).id;
       } else {
-        id = /^([0-9]){18}$/.test(message.params[0]) ? message.params[0] : message.member.id;
+        id = /^([0-9]){18}$/.test(message.parameters[0]) ? message.parameters[0] : message.member.id;
       }
-    } else if (isNaN(message.params.join(' '))) {
+    } else if (isNaN(message.parameters.join(' '))) {
       const members = message.guild.members.cache;
-      message.params = message.params.join(' ').toLowerCase();
+      const username = message.parameters.join(' ').toLowerCase();
 
-      id = members.map((member) => member.user.username.toLowerCase()).includes(message.params)
-      ? members.find((member) => member.user.username.toLowerCase() === message.params).id
+      id = members.map(({ user }) => user.username.toLowerCase()).includes(username)
+      ? members.find(({ user }) => user.username.toLowerCase() === username).id
       : message.member.id;
     } else {
       id = message.member.id;
