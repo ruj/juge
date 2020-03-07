e6c6d1bb050c12bb8d4ee3be14aace81f6712a50
tenasatupitsyn <214782@bk.ru>
2020-03-07 19:11:28 -0300

    Commits moved into a multi-field adder

--- core/commands/utility/changelog.js
+++ core/commands/utility/changelog.js
@@ -19,16 +19,18 @@ module.exports = {
     : branch[0]
 
     const commits = (await GitHub.getRepository(JUGE_REPO_USERNAME, JUGE_REPO_NAME, `commits?sha=${branch.commit.sha}&per_page=NEED_THIS`)).slice(0, 10);
-    const embed = new client.MessageEmbed()
+
+    message.channel.send(new client.MessageEmbed()
       .setColor(client.utils.hexColor(message))
       .setTitle(`:twisted_rightwards_arrows: ${JUGE_REPO_NAME}:${branch.name}`)
       .setURL(`https://github.com/${JUGE_REPO_USERNAME}/${JUGE_REPO_NAME}/commits/${branch.name}`)
-
-      commits.map(({ sha, commit, html_url, committer }) => embed.addField(sha.slice(0, 7), [
-        `[\`${commit.message}\`](${html_url}) (${client.utils.days(commit.committer.date, { extended: false }) > 1 ? `${client.utils.days(commit.committer.date)} ago` : 'Today'})`,
-        `**Committer**: [${committer.login}](${committer.html_url})`
-      ].join('\n')));
-
-    message.channel.send(embed);
+      .addFields(commits.map(({ sha, commit, html_url, committer }) => ({
+        name: sha.slice(0, 7),
+        value: [
+          `[\`${commit.message}\`](${html_url}) (${client.utils.days(commit.committer.date, { extended: false }) > 0 ? `${client.utils.days(commit.committer.date)} ago` : 'Today'})`,
+          `**Committer**: [${committer.login}](${committer.html_url})`
+        ].join('\n')
+      })))
+    );
   }
 };
