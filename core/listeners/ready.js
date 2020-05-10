9450c76f78b283c10989e3b723c3a9d5261b9a1c
tenasatupitsyn <214782@bk.ru>
2020-05-10 06:04:53 -0300

    Presence for servers with a number of members above 25

--- core/listeners/ready.js
+++ core/listeners/ready.js
@@ -1,3 +1,5 @@
+const ACTIVITY_INTERVAL = 2;
+
 module.exports = (client) => {
 	client.user.setPresence({ activity: { name: `@${client.user.username}` } });
 	client.log(`${client.user.tag} (${client.user.id})`, { tags: ['ready'], color: 'green'});
@@ -10,12 +12,15 @@ module.exports = (client) => {
 			}, {
 				name: `${client.users.cache.filter((user) => !user.bot && user.presence.status !== 'offline').array().length} Users`,
 				type: 'WATCHING'
+			}, {
+				name: `${client.guilds.cache.filter(({ memberCount }) => memberCount > 25).size} Guilds`,
+				type: 'WATCHING'
 			}
 		];
 
-		presences.forEach((presence, index) => setTimeout(() => client.user.setPresence({ activity: presence }), (index * 60) * 1E3));
+		presences.forEach((presence, index) => setTimeout(() => client.user.setPresence({ activity: presence }), 60 * (ACTIVITY_INTERVAL * index) * 1E3));
 
-		setTimeout(activity, 60 * presences.length * 1E3);
+		setTimeout(activity, 60 * (ACTIVITY_INTERVAL * presences.length) * 1E3);
 	}, (Math.random() * 10).toFixed(3) * 1E3);
 
 	client.getAndStoreEmojis();
