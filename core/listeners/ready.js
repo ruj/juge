5c1cc3966d77f86fbe21ee017b042ce549666acc
tenasatupitsyn <214782@bk.ru>
2020-05-20 09:37:46 -0300

    Posting statistics to bots lists

--- core/listeners/ready.js
+++ core/listeners/ready.js
@@ -1,3 +1,5 @@
+const fetch = require('node-fetch');
+
 const ACTIVITY_INTERVAL = 2;
 
 module.exports = (client) => {
@@ -10,7 +12,7 @@ module.exports = (client) => {
 				name: client.utils.randomItem(client.config.prefixes),
 				type: 'PLAYING'
 			}, {
-				name: `${client.users.cache.filter((user) => !user.bot && user.presence.status !== 'offline').array().length} Users`,
+				name: `${client.users.cache.filter((user) => !user.bot && user.presence.status !== 'offline').size} Users`,
 				type: 'WATCHING'
 			}, {
 				name: `${client.guilds.cache.filter(({ memberCount }) => memberCount >= 25).size} Guilds`,
@@ -22,6 +24,24 @@ module.exports = (client) => {
 
 		setTimeout(activity, 60 * (ACTIVITY_INTERVAL * presences.length) * 1E3);
 	}, (Math.random() * 10).toFixed(3) * 1E3);
+
+	setTimeout(function postStats() {
+		if (process.env.ARCANE_BOT_CENTER) {
+			fetch(`https://arcane-botcenter.xyz/api/${client.user.id}/stats`, {
+				method: 'POST',
+				headers: { Authorization: process.env.ARCANE_BOT_CENTER },
+				body: {
+					server_count: client.guilds.cache.size,
+					shard_count: client.shard.count,
+					member_count: client.users.cache.size
+				}
+			})
+			.then(() => client.log('Posted statistics successfully', { tags: ['arcane-botcenter'], color: 'magenta' }))
+			.catch(() => client.log('Failed to post statistics', { tags: ['arcane-botcenter'], color: 'blue' }));
+		}
+
+		setTimeout(postStats, 1800000);
+	}, 60000);
 
 	client.getAndStoreEmojis();
 };
